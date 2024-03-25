e731f3758f90390b0175ce444394b952f7b14bd2
Timishot <timidotcom58@gmail.com>
2024-03-25 23:59:30 +0100

    push

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+"""
+lockboxes in python3
+"""
+
 
 def canUnlockAll(boxes):
-    if not boxes:
+    """
+    Function that checks with a boolean value if the list type and
+    length to invoke two for iterations: one to traverse the list
+    and the other to compare if the key is idx or not in order to open
+    """
+    if not isinstance(boxes, list):
         return False
-    
-    visited = set()
-    queue = deque([0])  # Start with the first box (index 0)
-    visited.add(0)
 
-    while queue:
-        current_box = queue.popleft()
-        keys = boxes[current_box]
+    for key in range(1, len(boxes) - 1):
+        boxes_checked = any(key in box and key != idx for idx,
+                            box in enumerate(boxes))
+        if not boxes_checked:
+            return False
 
-        for key in keys:
-            if key < len(boxes) and key not in visited:
-                visited.add(key)
-                queue.append(key)
-
-    return len(visited) == len(boxes)
+    return True
