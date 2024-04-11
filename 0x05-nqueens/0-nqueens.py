d5c44988017f97fb6383941e23365084797edca2
Timishot <timidotcom58@gmail.com>
2024-04-11 23:25:37 +0100

    working

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""N queens puzzle"""
+import sys
+
+
+def is_safe(board, row, col, n):
+    """Check if a queen can be placed on board"""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(board, row, n):
+    """Solve n queens puzzle"""
+    if row == n:
+        print([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+
+def nqueens(n):
+    """Init n queens puzzle"""
+    if not n.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    n = int(n)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    nqueens(sys.argv[1])
